a7bb2bab86c49d177c10f76ed765644b959cc2b2
이준열(Junyeol Lee)/FE개발팀/11ST <jylee11st@sk.com>
2023-10-15 16:07:40 +0900

    feat: prettier

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
+const path = require('path');
+const AutoLoad = require('@fastify/autoload');
 
 // Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {}
+module.exports.options = {};
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -16,13 +16,13 @@ module.exports = async function (fastify, opts) {
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    options: Object.assign({}, opts),
+  });
+};
